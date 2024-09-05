85a7bbeeec4181836f2c86f5c71223c10f12d658
emanbruiz <abarahonar1@miumg.edu.gt>
2024-09-05 16:15:04 -0600

    Correacciones

--- Script.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE registros (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
-    apellido VARCHAR(50) NOT NULL,
-    edad INT NOT NULL,
-    puesto VARCHAR(50),
-    descripcion TEXT,
-    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
